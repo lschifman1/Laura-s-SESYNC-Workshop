fe8b33b51fb9bd24697229b44f7c889b0db598f3
sesync <sesync@sesync.org>
2016-09-14 10:32:39 -0400

    New user name and email added

--- lesson-1.R
+++ lesson-1.R
@@ -1,8 +1,8 @@
 ## Configure git
 
-system("git config --global user.name ...")
-system("git config --global user.email ...")
+system("git config --global user.name 'Laura Schifman'")
+system("git config --global user.email laura.schifman@gmail.com")
 
 ## Set a new origin URL
 
-system('git remote set-url origin ...')
+system('git remote set-url origin https://github.com/lschifman1/Laura-s-SESYNC-Workshop.git')
