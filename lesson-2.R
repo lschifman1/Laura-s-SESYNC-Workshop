b03ffd6b7d72629c6556f8912d1dc60d400ca321
sesync <sesync@sesync.org>
2016-09-14 16:00:14 -0400

    completed all exercises

--- lesson-2.R
+++ lesson-2.R
@@ -7,51 +7,56 @@ vals <- seq(from=1,
 
 ## Vectors
 
-counts ...
+counts <- c(4,3,7,5)
 
 ## Lists
 
-... <- list(...)
-... <- ...(list(1, 2), c(3, 4))
+x <- list(list(1,2), c(3,4))
+y <- c(list(1, 2), c(3, 4))
 
 ## Factors
-
-education <- ...(c("college", "highschool", "college", "middle"),
-                 ... = c("middle", "highschool", ...),
-                 ...)
+education <- factor(
+  c("college", "highschool", "college", "middle"),
+  levels = c("middle", "highschool", "college"), 
+  ordered=T)
 
 ## Data Frames
 
-... data.frame(...)
+df <- data.frame(education, counts)
 
 ## Exercise 1
 
-...
+species<-c("cat","dog","horse")
+count<-c(3,5,1)
+data<-data.frame(species,count)
+
 
 ## Names
 
-names(...) <- ...
+names(df) <- c("ed","ct")
 
 ## Subsetting ranges
 
 days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-weekdays <- ...
-...
+weekdays <- days[2:6]
+weekends<-days[c(1,7)]
 
 ## Exercise 2
 
-...
+weekdays2<-days[c(-1,-7)]
+weekdays3<-days[c(2,4,6)]
+MWF<-days[seq(from=2,to=6,by=2)]
 
 ## Anatomy of a function
 
-function(...) {
-  ...
-  return(...)
+first <-function(x) {
+  result<-x[1,1]
+  return(result)
 }
 
 ## Exercise 3
 
-...
+df[2:3,1]
 
 ## Distributions and statistics
 
